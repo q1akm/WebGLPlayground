ad4066eb1fcb8da91e4223b36dd743e981202c81
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-09-27 14:25:00 +0700

    testing models

--- src/test_models/script.js
+++ src/test_models/script.js
@@ -1,14 +1,33 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(12, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 4);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let loadedModel;
+const gltfloader = new GLTFLoader();
+gltfloader.load('./models/utility_box_01_1k/utility_box_01_1k.gltf', (gltfScene) => {
+    loadedModel = gltfScene;
+
+    scene.add(gltfScene.scene)
+})
 
 
-/**
- * Base
- */
-// Debug
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+  animate();
 
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
+  // Adjust the canvas when the window is resized
+  window.addEventListener("resize", () => {
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
